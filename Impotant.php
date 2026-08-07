b214227dbadece94d8dbf145ffde02d756481cff
MayankSaini001 <mayanksaini200@gmail.com>
2026-08-07 11:22:14 +0530

    find max no. in array

--- Impotant.php
+++ Impotant.php
@@ -25,3 +25,20 @@ return reversed;
 console.log(reverseString("Mango"));
 
 
+/*==== Find Maximum number in array ====*/
+
+function findMax(arr) {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return max;
+}
+
+console.log(findMax([10, 5, 20, 8]));
+
+
